c623823f987a0dfedd3eb1768bfc6d90f3e96a01
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-09-25 19:32:28 -0300

    feat(Implementação): de funções
    para pegar dias, pedidos, nomes dos clientes e dicionario com nome, dias e comidas por cada cliente

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,50 @@
+import csv
+
+
+# Requisito 1 realizado com a ajuda do aluno João Lenny, Turma 16 Tribo A.
+def get_days_that_the_restaurant_opens(file_lines):
+    days = set()
+
+    for line in file_lines:
+        days.add(line[2])
+    
+    return days
+
+
+def get_restaurant_menu(file_lines):
+    menu = set()
+
+    for line in file_lines:
+        menu.add(line[1])
+
+    return menu
+
+
+def get_restaurant_customers(file_lines):
+    customers = set()
+    customers_dicts = dict()
+
+    for line in file_lines:
+        customers.add(line[0])
+    
+    for customer in customers:
+        customers_dicts[customer] = {'days': [], 'foods': []}
+
+    return customers_dicts
+
+
+def implementing_days_and_food(file_lines):
+    customers_dicts = get_restaurant_customers(file_lines)
+    customers = list(customers_dicts.keys())
+
+    for line in file_lines:
+        for customer in customers:
+            if customer in line:
+                customers_dicts[customer]['days'].append(line[2])
+                customers_dicts[customer]['foods'].append(line[1])
+
+    return customers_dicts    
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
