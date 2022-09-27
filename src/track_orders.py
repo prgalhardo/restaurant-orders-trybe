1d98700a3b2ccaace1af0387bb1a14b2139f1b49
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-09-26 23:19:51 -0300

    feat(Implementação): track_orders.py
    Função para verificar prato nunca pedido por cliente

--- src/track_orders.py
+++ src/track_orders.py
@@ -21,7 +21,17 @@ class TrackOrders:
         return most_ordered_dish
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        all_dishes = set()
+        customer_dishes = set()
+
+        for line in self.orders:
+            all_dishes.add(line[1])
+            if line[0] == customer:
+                customer_dishes.add(line[1])
+
+        never_ordered = all_dishes.difference(customer_dishes)
+
+        return never_ordered
 
     def get_days_never_visited_per_customer(self, customer):
         pass
